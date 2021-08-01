9154f2c1cfbe7cdf4ecdc33ece6dcd6d89b0eb55
Manoj Kunapalli <manoj.bemt.14@gmail.com>
2021-08-01 21:55:44 +0000

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/Desktop/sofar/src
-# Build directory: /root/Desktop/sofar/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
